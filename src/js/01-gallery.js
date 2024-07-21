7eb2d80e146e4854c8cbde3b54d23e2d33d19130
DorinGI <158717197+DorinGI@users.noreply.github.com>
2024-07-21 10:24:06 +0200

    HW 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const listElement = document.querySelector(".gallery");
+const photoMarkup = createGalleryItem(galleryItems);
+
+function createGalleryItem(element) {
+    return element
+        .map(({ preview, original, description }) => {
+            return `<a class="gallery__link" href="${preview}">
+                <img class="gallery__image" src="${original}" alt="${description}" />
+                </a>
+            `;
+        })
+    .join(" ");
+}
+
+listElement.insertAdjacentHTML("beforeend", photoMarkup);
+
+const galleryHandler = new SimpleLightbox(".gallery a", {
+        captionPosition: "bottom",
+        captionsData: "alt",
+        captionDelay: 250,
+});
+galleryHandler.on("show.simplelightbox");
